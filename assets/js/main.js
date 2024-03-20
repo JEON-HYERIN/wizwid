6fa4212b1cc78069cebd8c89e366303388cffed3
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-20 17:27:49 +0900

    Fix: keyworkd > 마크업

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ new Swiper('.section-benefit .benefit-list-wrap', {
   },
 });
 
-new Swiper('.section-wish .wish-list-wrap', {
+new Swiper('.section-wish .product-list-wrap', {
   slidesPerView: 'auto',
   navigation: {
     prevEl: '.section-wish .btn-prev',
@@ -52,8 +52,66 @@ new Swiper('.section-wish .wish-list-wrap', {
   },
 });
 
+const lookbookSlide1 = new Swiper('.section-lookbook .lookbook-list-wrap', {
+  // autoplay: {
+  //   delay: 3000
+  // },
+  loop: true,
+  speed: 600,
+  slidesPerView: 3,
+  // spaceBetween: 60,
+  centeredSlides: true,
+  navigation: {
+    prevEl: '.section-lookbook .btn-prev',
+    nextEl: '.section-lookbook .btn-next',
+  },
+  on: {
+    slideChange: function() {
+      // console.log('hello');
+    }
+  }
+});
+
+// const lookbookSlide2 = new Swiper('.section-lookbook .product-list-wrap', {
+//   speed: 600,
+//   slidesPerView: 'auto',
+//   pagination: {
+//     el: ".section-lookbook .swiper-pagination",
+//     type: "progressbar",
+//   },
+//   on:{
+//     'reachEnd':function(){
+//       cnt = this.slides.length
+//       realIdx = this.realIndex+1;
+//       if(cnt === realIdx){
+//         lookbookSlide1.slideNext();
+//       }
+//     },
+//     'reachBeginning':function(){
+//       realIdx = this.realIndex;
+//       if(0 === realIdx){
+//         lookbookSlide1.slidePrev();
+//       }
+//     }
+//   }
+// });
+
+// lookbookSlide1.on('slideChange', function() {
+//   idx = lookbookSlide1.realIndex;
+//   console.log(idx);
+//   lookbookSlide2.slideToLoop(idx)
+// })
 
 
+new Swiper('.section-keyword .keyword-list-wrap', {
+  slidesPerView: 'auto',
+  loop: true,
+  speed: 600,
+  navigation: {
+    prevEl: '.section-keyword .btn-prev',
+    nextEl: '.section-keyword .btn-next',
+  },
+});
 
 $(window).scroll(function() {
   const scrollValue = $(document).scrollTop();
